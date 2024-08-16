8095cf3ec4c508d743d273796ac4028af4fc52c3
Neptun404 <shadowova329@gmail.com>
2024-08-16 23:11:59 +0800

    added additional unit tests for user registration

--- backend/tests/user.test.ts
+++ backend/tests/user.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@jest/globals';
 import 'dotenv/config';
-
+import db from '../src/db';
 
 test('[R001] user registration should fail if inputs are empty', async () => {
     const response = await fetch(`http://${process.env.HOST}:${process.env.PORT}/user/register`, {
@@ -30,4 +30,41 @@ test('[R002] Test user registration should fail if inputs are empty spaces', asy
     })
 
     expect(response.status).toBe(400); // Assuming 400 is the status for a bad request
+})
+
+test('[R003] Test user registration should fail if username already exists', async () => {
+    const response = await fetch(`http://${process.env.HOST}:${process.env.PORT}/user/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: 'neptun',
+            password: 'neptun@123'
+        })
+    })
+
+    expect(response.status).toBe(409);
+})
+
+test('[R004] Test user registration should succeed', async () => {
+    // Delete user if it already exists
+    await db.user.delete({
+        where: {
+            username: 'neptun'
+        }
+    })
+    
+    const response = await fetch(`http://${process.env.HOST}:${process.env.PORT}/user/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: 'neptun',
+            password: 'neptun@123'
+        })
+    })
+
+    expect(response.status).toBe(201);
 })
