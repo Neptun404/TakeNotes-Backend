f2f295d5d77f4a0310e64872360a75c3684b0cdf
Neptun404 <shadowova329@gmail.com>
2024-08-16 15:09:02 +0800

    added a simple error handler

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Errback, NextFunction, Request, Response, Router } from 'express';
 import { login, register } from '../controllers/user.controller'
 
 const router = Router()
@@ -6,3 +6,8 @@ const router = Router()
 export default router
     .post('/login', login)
     .post('/register', register)
+    .use(function (err: Errback, req: Request, res: Response, next: NextFunction) {
+        res.status(400).json({
+            err
+        })
+    })
