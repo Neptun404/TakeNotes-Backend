0a9420fd5f993ca2dac2773fa4b0c327e0cf5c6c
Neptun404 <shadowova329@gmail.com>
2024-08-16 14:51:49 +0800

    test if connection works before app is started

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,21 @@
 import 'dotenv/config';
 import express from 'express';
+import db from './db';
 
 const app = express()
+
+// Test if database connection is working
+db.$connect()
+    .then(async _ => {
+        console.log('Prisma connected')
+        await db.$disconnect()
+    })
+    .catch(async err => {
+        console.error(err)
+        await db.$disconnect()
+        process.exit(1)
+    })
+
 /**
  * App configuration here
  */
