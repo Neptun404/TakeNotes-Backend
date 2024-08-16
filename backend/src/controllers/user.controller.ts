460576caada522447fc3fbe84ece3dab74bf7c75
Neptun404 <shadowova329@gmail.com>
2024-08-16 21:30:01 +0800

    implemented logic for user login

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,9 +2,41 @@ import { NextFunction, Request, Response } from 'express'
 import db from '../db';
 import bcrypt from 'bcrypt';
 import createError from 'http-errors';
+import jwt from 'jsonwebtoken';
 
-export async function login(req: Request, res: Response) {
-    res.send('Login user');
+export async function login(req: Request, res: Response, next: NextFunction) {
+    const { username, password } = req.body
+
+    // Check if username and password are not empty
+    if (
+        !username ||
+        !password ||
+        !username.trim() ||
+        !password.trim()
+    ) {
+        return next(createError(400, 'Username and password are required'))
+    }
+
+    // Fetch user from database
+    const user = await db.user.findFirst({
+        where: {
+            username: username
+        }
+    })
+
+    // If user does not exist or password is incorrect, return error
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+        return next(createError(401, 'Invalid username or password'))
+    }
+
+    // Create a JWT token with user id and secret for authentication
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '1d' })
+
+    // Send response with success message and status code
+    res.json({
+        message: 'Login succesful',
+        token
+    });
 }
 
 export async function register(req: Request, res: Response, next: NextFunction) {
