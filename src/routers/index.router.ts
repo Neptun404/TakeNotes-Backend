c98eb11bfc2e732bea54fd6926b1c23fe62778a3
Neptun404 <shadowova329@gmail.com>
2024-08-17 22:18:19 +0800

    registered note router into application

--- src/routers/index.router.ts
+++ src/routers/index.router.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import userRouter from "./user.router";
+import noteRouter from "./note.router";
 
 const router = Router()
 
 export default router
     .use('/user/', userRouter)
+    .use('/note', noteRouter)
