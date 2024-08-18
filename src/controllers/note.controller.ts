71d3b7c6b366641cb0df07fa8032f1be6fad9339
Neptun404 <shadowova329@gmail.com>
2024-08-18 09:35:18 +0800

    implemented controller for updating note

--- src/controllers/note.controller.ts
+++ src/controllers/note.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import createError from 'http-errors';
 import db from '../db';
+import noteServices, { DatabaseError, NoteNotFoundError } from '../services/note.service';
 
 export async function getNote(req: Request, res: Response, next: NextFunction) {
     const { userId } = res.locals as { userId: number };
@@ -93,7 +94,49 @@ export async function createNote(req: Request, res: Response, next: NextFunction
     }
 }
 
-export async function updateNote(req: Request, res: Response, next: NextFunction) { }
+export async function updateNote(req: Request, res: Response, next: NextFunction) {
+    const { userId } = res.locals as { userId: number };
+    const { id: noteId } = req.params;
+
+    // Check if id is valid note id
+    if (!Number.isInteger(parseInt(noteId))) {
+        return next(createError(400, 'Invalid note id'))
+    }
+
+    const { title, content } = req.body
+
+    // Validate title is not empty
+    if (
+        !title ||
+        (title as string).trim() === ''
+    ) {
+        return next(createError(400, 'Note title cannot be empty'))
+    }
+
+    try {
+        // Update note in database
+        const updatedNote = await noteServices.updateNote(parseInt(noteId), userId, { title, content })
+
+        // Send response with success message and resource
+        res.status(200).json({
+            "status": "success",
+            "message": "Note updated",
+            "data": updatedNote
+        })
+    } catch (error) {
+        if (error instanceof NoteNotFoundError)
+            return next(createError(404, 'Note not found'))
+        else if (error instanceof DatabaseError) {
+            console.error('Database error:', error.dbError);
+            return next(createError(500, error.message))
+        }
+        else {
+            console.error('Unexpected error:', error);
+            return next(createError(500, 'Internal server error'))
+        }
+
+    }
+}
 
 export async function deleteNote(req: Request, res: Response, next: NextFunction) { }
 
