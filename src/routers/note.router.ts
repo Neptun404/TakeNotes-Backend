57067d5ed2f9df4b560625a41d6a33e6802a12b8
Neptun404 <shadowova329@gmail.com>
2024-08-19 17:09:06 +0800

    use default exported functions for unimplemented controllers

--- src/routers/note.router.ts
+++ src/routers/note.router.ts
@@ -1,5 +1,5 @@
 import { Router, NextFunction, Request, Response } from "express";
-import { createNote, deleteNote, getNote, getNotes, updateNote } from '../controllers/note.controller';
+import controllers, { getNote, getNotes } from '../controllers/note.controller';
 import jwtAuth from "../middlewares/auth.jwt";
 import { HttpError } from 'http-errors'
 
@@ -9,9 +9,9 @@ export default router
     .use(jwtAuth)
     .get('/:id', getNote)
     .get('/', getNotes)
-    .post('/', createNote)
-    .put('/:id', updateNote)
-    .delete('/:id', deleteNote)
+    .post('/', controllers.createNote)
+    .put('/:id', controllers.updateNote)
+    .delete('/:id', controllers.deleteNote)
     .use(function (err: HttpError, req: Request, res: Response, next: NextFunction) {
         res.status(err.statusCode).json({
             message: err.message
