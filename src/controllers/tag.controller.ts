524e44bf7decbc5d2cb7529cc4b3b4de5fc9d7b0
Neptun404 <shadowova329@gmail.com>
2024-08-23 16:48:51 +0800

    Return internal server error instead of error

--- src/controllers/tag.controller.ts
+++ src/controllers/tag.controller.ts
@@ -14,6 +14,6 @@ export async function getTags(req: Request, res: Response, next: NextFunction) {
         })
     } catch (error) {
         if (error instanceof DatabaseError) return next(createError(500, 'Database error'))
-        else return next(createError(500, error))
+        else return next(createError(500, 'Internal Server Error'))
     }
 }
