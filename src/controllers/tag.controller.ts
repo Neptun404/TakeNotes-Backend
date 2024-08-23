b5bb46301417f5b1232f0c5dfb14ab05e87d89eb
Neptun404 <shadowova329@gmail.com>
2024-08-23 16:37:44 +0800

    implemented controller for tag route

---
+++ src/controllers/tag.controller.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import tagService from "../services/tag.service";
+import { DatabaseError } from "../errors/DatabaseErrors";
+import createError from '../utils/createError';
+
+export async function getTags(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { ownerId } = res.locals;
+
+        res.status(200).json({
+            status: 'success',
+            message: 'Tags retrieved successfully',
+            data: await tagService.getTags(ownerId)
+        })
+    } catch (error) {
+        if (error instanceof DatabaseError) return next(createError(500, 'Database error'))
+        else return next(createError(500, error))
+    }
+}
