36df748e541834b5921fd1369664b275a36cfc00
Neptun404 <shadowova329@gmail.com>
2024-08-23 16:49:08 +0800

    switched getTags from tag.service to tag.controller

--- src/routers/tag.router.ts
+++ src/routers/tag.router.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response, Router } from "express";
 import jwtAuth from '../middlewares/auth.jwt';
 import { HttpError } from 'http-errors';
-import { getTags } from "../services/tag.service";
+import { getTags } from "../controllers/tag.controller";
 
 const router = Router()
 
