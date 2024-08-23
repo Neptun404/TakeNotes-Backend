25789bba46354c4ab8a1629768d787dd44b56db0
Neptun404 <shadowova329@gmail.com>
2024-08-23 16:37:34 +0800

    added tests for tag service

--- tests/tag/tag.service.test.ts
+++ tests/tag/tag.service.test.ts
@@ -1,19 +1,40 @@
 import db from "../../src/db";
 import {
-    createTags
+    createTags,
+    getTags
 } from '../../src/services/tag.service'
 
 jest.mock('../../src/db', () => ({
     __esModule: true,
     default: {
         tag: {
-            createTags: jest.fn()
+            createTags: jest.fn(),
+        },
+        note: {
+            findMany: jest.fn()
         }
     }
 }))
 
 
-const mockCreateTags = db.tag.createMany as jest.Mock;
+const mockGetNoteTags = db.note.findMany as jest.Mock;
+
+describe('Test cases for getTags', () => {
+    it('should return empty array if no tags found', async () => {
+        mockGetNoteTags.mockResolvedValue([])
+
+        const a = getTags(2);
+        await expect(a).resolves.toEqual([])
+    })
+
+    it('should return array of tags created by the user', async () => {
+        mockGetNoteTags.mockResolvedValue(['tag1', 'tag2', 'delish'])
+
+        const a = getTags(2);
+        expect(a).resolves.toEqual(['tag1', 'tag2', 'delish'])
+    })
+})
+
 
 it('', () => {
     expect(true)
