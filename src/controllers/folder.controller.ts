2f1900c09d017055c1e8397996f1cbae9deed3e6
Neptun404 <shadowova329@gmail.com>
2024-08-26 17:02:51 +0800

    Implemented createFolder controller

--- src/controllers/folder.controller.ts
+++ src/controllers/folder.controller.ts
@@ -1,6 +1,19 @@
 import { Request, Response, NextFunction } from "express"
+import folderService from "../services/folder.service";
+import { InvalidNoteID, validateNoteID } from "./note.controller";
+import { DatabaseError, NoteNotFoundError } from "../errors/DatabaseErrors";
+import createError from "../utils/createError";
+
+class ValidationError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.message = message;
+        this.name = 'ValidationError'
+    }
+}
 
 async function getFolder(req: Request, res: Response, next: NextFunction) {
+
     throw new Error('Endpoint not implemented')
 }
 
@@ -9,7 +22,41 @@ async function getFolders(req: Request, res: Response, next: NextFunction) {
 }
 
 async function createFolder(req: Request, res: Response, next: NextFunction) {
-    throw new Error('Endpoint not implemented')
+    try {
+        // Validate request body
+        const { title } = req.body
+        if (!title || (title as string).trim() === '')
+            throw new ValidationError('Invalid folder title');
+
+        // Create folder
+        const { userId } = res.locals;
+        const { notes } = req.body
+
+        if (!notes) throw new ValidationError('notes field does not exists')
+        else if (!Array.isArray(notes)) throw new ValidationError('notes must be an array')
+
+        const _notes: { id: number }[] = notes.map(noteId => {
+            return { id: validateNoteID(noteId) }
+        })
+
+        const folder = await folderService.createFolder(userId, {
+            title: title,
+            notes: _notes
+        })
+
+        res.status(200).json({
+            staus: 'success',
+            message: 'Folder created',
+            data: folder
+        })
+    } catch (error) {
+        console.error((error as Error).message);
+        if (error instanceof ValidationError) return next(createError(400, error.message))
+        else if (error instanceof InvalidNoteID) return next(createError(400, error.message))
+        else if (error instanceof NoteNotFoundError) return next(createError(400, error.message))
+        else if (error instanceof DatabaseError) return next(createError(500, 'Database error'))
+        else return next(createError(500, 'Internal Server error'))
+    }
 }
 
 async function updateFolder(req: Request, res: Response, next: NextFunction) {
